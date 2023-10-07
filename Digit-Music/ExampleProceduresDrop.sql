e03b4accfb2fee772c1033e221f135d151a87523
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-07 23:44:31 +0200

    Update ExampleProceduresDrop.sql

--- Digit-Music/ExampleProceduresDrop.sql
+++ Digit-Music/ExampleProceduresDrop.sql
@@ -4,8 +4,8 @@ DROP PROCEDURE [UpdateTrackPrice]
 GO
 DROP PROCEDURE [DeletePlaylist]
 GO
-DROP PROCEDURE [CountInvoicesCustomer]
+DROP PROCEDURE [CountOfCustomerInvoice]
 GO
 DROP PROCEDURE [GetCustomersByEmployeeName]
 GO
-DROP PROCEDURE [GetPlaylistsNamesByTrack];
+DROP PROCEDURE [GetPlaylistsByTrack];
