5c7ec3581ddd8a260b4e6bd486fca66abbc44540
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-07 13:36:04 +0200

    Create ExampleProceduresDrop.sql

---
+++ Digit-Music/ExampleProceduresDrop.sql
@@ -0,0 +1,11 @@
+USE [DigitMusic]
+GO
+DROP PROCEDURE [UpdateTrackPrice]
+GO
+DROP PROCEDURE [DeletePlaylist]
+GO
+DROP PROCEDURE [CountInvoicesCustomer]
+GO
+DROP PROCEDURE [GetCustomersByEmployeeName]
+GO
+DROP PROCEDURE [GetPlaylistsNamesByTrack];
